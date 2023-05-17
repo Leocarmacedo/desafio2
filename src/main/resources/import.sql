7c9463a4dfc7a4688682056710dce27c66d40033
Leocarnauba <leocarmacedo@hotmail.com>
2023-05-17 16:28:03 -0300

    Corrigindo Seed

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -6,7 +6,7 @@ INSERT INTO tb_atividade(nome, descricao, preco, categoria_id) VALUES ('Oficina 
 
 INSERT INTO tb_bloco(inicio, fim, atividade_id) VALUES (TIMESTAMP WITH TIME ZONE '2017-09-25T08:00:00Z', TIMESTAMP WITH TIME ZONE '2017-09-25T11:00:00Z', 1);
 INSERT INTO tb_bloco(inicio, fim, atividade_id) VALUES (TIMESTAMP WITH TIME ZONE '2017-09-25T14:00:00Z', TIMESTAMP WITH TIME ZONE '2017-09-25T18:00:00Z', 2);
-INSERT INTO tb_bloco(inicio, fim, atividade_id) VALUES (TIMESTAMP WITH TIME ZONE '2017-09-26T08:00:00Z', TIMESTAMP WITH TIME ZONE '2017-09-26T11:00:00Z', 3);
+INSERT INTO tb_bloco(inicio, fim, atividade_id) VALUES (TIMESTAMP WITH TIME ZONE '2017-09-26T08:00:00Z', TIMESTAMP WITH TIME ZONE '2017-09-26T11:00:00Z', 2);
 
 
 INSERT INTO tb_participante(nome, email) VALUES ('José Silva', 'jose@gmail.com');
